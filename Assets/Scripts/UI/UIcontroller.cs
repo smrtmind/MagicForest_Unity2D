5e4382ff289ab42937ca693efd0907aa07fedf72
Oleg Gvozdetskyi <oleg.gvozdetskyi@gmail.com>
2022-06-18 16:15:18 +0300

    do animated menu without animator

--- Assets/Scripts/UI/UIcontroller.cs
+++ Assets/Scripts/UI/UIcontroller.cs
@@ -1,35 +1,100 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace Scripts.UI
 {
-    public class UIcontroller : MonoBehaviour
+    public class UiController : MonoBehaviour
     {
-        [SerializeField] private Image[] _buttonImage;
-        [SerializeField] private Text[] _buttonText;
+        //[SerializeField] private Color _buttonsAndTextColor;
+        [SerializeField] private Button[] _buttons;
+
+        //[SerializeField] private Image[] _buttonImage;
+        //[SerializeField] private Text[] _buttonText;
+        [SerializeField] private float _menuSlideSpeed = 1f;
 
         private Color _defaultButtonColor;
-        private Color _defaultButtonTextColor;
+        private CanvasScaler _canvas;
+        private float _canvasWidth;
+        private float _canvasHeight;
+        private GridLayoutGroup _grid;
+        private Vector3 _defaultMenuPosition;
+
+        private bool _showMenu;
+        private bool _showSettings;
 
         private void Awake()
         {
-            _defaultButtonColor = _buttonImage[0].color;
-            _defaultButtonTextColor = _buttonText[0].color;
+            _defaultMenuPosition = transform.position;
+            _grid = FindObjectOfType<GridLayoutGroup>();
+            _canvas = FindObjectOfType<Canvas>().GetComponent<CanvasScaler>();
+            _canvasWidth = _canvas.referenceResolution.x;
+            _canvasHeight = _canvas.referenceResolution.y;
+
+            _defaultButtonColor = _buttons[0].transform.Find("Text").GetComponent<Text>().color;
+        }
+
+        private void Start()
+        {
+            _grid.cellSize = new Vector2(_canvasWidth, _canvasHeight);
         }
 
         public void OnButtonPressed(int buttonIndex)
         {
-            _buttonImage[buttonIndex].color = Color.green;
-            _buttonText[buttonIndex].color = Color.white;
+            _buttons[buttonIndex].GetComponent<Image>().color = _defaultButtonColor;
+            _buttons[buttonIndex].transform.Find("Text").GetComponent<Text>().color = Color.white;
+
+            //_buttonImage[buttonIndex].color = Color.green;
+            //_buttonText[buttonIndex].color = Color.white;
         }
 
         public void OnButtonReleased()
         {
-            foreach (var buttonImage in _buttonImage)
-                buttonImage.color = _defaultButtonColor;
+            foreach (var button in _buttons)
+            {
+                button.GetComponent<Image>().color = Color.white;
+                button.transform.Find("Text").GetComponent<Text>().color = _defaultButtonColor;
+            }
+            //    buttonImage.color = _defaultButtonColor;
 
-            foreach (var buttonText in _buttonText)
-                buttonText.color = _defaultButtonTextColor;
+            //foreach (var buttonText in _buttonText)
+            //    buttonText.color = _defaultButtonTextColor;
+        }
+
+        public void ButtonsIsActive(bool state)
+        {
+            foreach (var button in _buttons)
+                button.GetComponent<EventTrigger>().enabled = state;
+        }
+
+        public void ShowMenu() => _showMenu = true;
+
+        public void ShowSettings() => _showSettings = true;
+
+        private void Update()
+        {
+            if (_showSettings)
+            {
+                if (gameObject.transform.position.x <= -_canvasWidth)
+                {
+                    ButtonsIsActive(true);
+                    _showSettings = false;
+                    return;
+                }
+
+                gameObject.transform.position = new Vector3(transform.position.x - _menuSlideSpeed, transform.position.y);
+            }
+            if (_showMenu)
+            {
+                if (gameObject.transform.position == _defaultMenuPosition)
+                {
+                    ButtonsIsActive(true);
+                    _showMenu = false;
+                    return;
+                }
+
+                gameObject.transform.position = new Vector3(transform.position.x + _menuSlideSpeed, transform.position.y);
+            }
         }
     }
 }
